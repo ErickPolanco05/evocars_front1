edd55c99d780d375f7e688953d5571cbd67229b9
marietaaaaa03 <marietamartinez23022018@gmail.com>
2024-11-24 15:01:10 -0600

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://tu-backend-api-url.com/api', // Reemplaza con la URL de tu API backend
+  baseURL: 'https://evocars.vercel.app/api', // Reemplaza con la URL de tu API backend
   timeout: 10000, // Tiempo de espera de 10 segundos
   headers: {
     'Content-Type': 'application/json',
